2242482b16ec63cb0c2eaea155c26af6966b25ee
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-10-19 17:14:02 +0200

    Seed Managing
    Ora il seed viene settato solo una volta all'inizio del programma.

--- compitini/1_Cholesky-Factorization/tester/data_generator.py
+++ compitini/1_Cholesky-Factorization/tester/data_generator.py
@@ -2,15 +2,14 @@ from typing import Tuple
 import numpy as np
 
 
+__seed_setted = False
+
+
 def __generate_A(size=10, seed=None) -> np.ndarray:
     '''
         Genera una matrice Quadrata, Simmetrica e Definita Positiva di dimensione
         size.
     '''
-
-    # se esplicitamente passato, viene settato il seed
-    if seed is not None:
-        np.random.seed(seed)
 
     # magic ✨
     A = np.random.rand(size, size)
@@ -24,10 +23,6 @@ def __generate_b(size=10, seed=None) -> np.array:
         Genera il vettore dei termini noti
     '''
 
-    # se esplicitamente passato, viene settato il seed
-    if seed is not None:
-        np.random.seed(seed)
-
     b = np.random.rand(size)
 
     return b 
@@ -38,4 +33,15 @@ def generate_data(size=10, seed=None) -> Tuple[np.ndarray, np.array]:
         Ritorna una tupla con i dati del sistema lineare A e b.
     '''
     
-    return (__generate_A(size, seed), __generate_b(size, seed))
+    global __seed_setted
+
+    # se esplicitamente passato, viene settato il seed
+    if seed is not None and not __seed_setted:
+        np.random.seed(seed)
+
+    A =  __generate_A(size, seed)
+    b = __generate_b(size, seed)
+
+    __seed_setted = True    # non setta più il seed in futuro
+    
+    return (A, b)
