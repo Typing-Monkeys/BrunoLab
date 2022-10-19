081d78c3621e79c5d2418f3030472b87a92fd66a
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-10-19 17:54:52 +0200

    Cholesky Execution Time
    Creata una funzione per risolvere un dato sistema lineare
    che astrae da tutta l'implementazione.
    Creata una funzione che prova a risolvere sistemi lineari sempre più grandi.
    Creata una classe in gradi di gestire il comportamento verbose.

--- compitini/1_Cholesky-Factorization/main.py
+++ compitini/1_Cholesky-Factorization/main.py
@@ -5,7 +5,11 @@ import tester as Tester
 
 
 def main():
+    # Tester.find_limit(seed=20)
+    
     A, b = Tester.generate_data(size=5, seed=20)
+
+    _ = Tester.solve(A, b)
 
     """ A = np.array([
         [5.2, 3, 0.5, 1, 2],
@@ -15,34 +19,8 @@ def main():
         [2,0,3,2.6,15]
     ], dtype=float)
 
-    b = np.array([9.45, -12.20, 7.78, -8.1, 10.0], dtype=float) # termine noto """
-
-    print(f"A:\n{A}\n")
-    print(f"b:\n{b}\n")
-
-    # --- Decomposizione della matrice A in LU --- #
-    print("CHOLESKY FACTORIZATION ...")
-
-    execution_time, L = Tester.get_execution_time(Cholesky_factorization.compute, [A])
-    
-    if L is None:
-        print("Impossibile scomporre la matrice data !!")
-        return -1
-    
-    print(f"L:\n{L}\n")
-    print(f"Il risultato è corretto ?: {'✅' if (Cholesky_factorization.is_correct_solution(A, L)) else '❌'}")
-    print(f"Tempo di esecuzione: {execution_time}")
-    print('\n')
-
-    # --- Risoluzione del Sistema Lineare --- #
-    print("SOLVING LINEAR SYSTEM ...")
-
-    execution_time, x = Tester.get_execution_time(Linear_sistem.solve, [L, b])
-
-    print(f"x: \n{x}\n")
-    print(f"Il risultato è corretto ?: {'✅' if (Linear_sistem.is_correct_solution(A, x, b)) else '❌'}")
-    print(f"Tempo di esecuzione: {execution_time}")
-
+    b = np.array([9.45, -12.20, 7.78, -8.1, 10.0], dtype=float) # termine noto 
+    """
 
 if __name__ == "__main__":
     main()
