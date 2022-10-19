0d847d07a11b20ff7162e9edd3cd35ac72489794
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-10-19 18:05:29 +0200

    CLI Arguments
    Aggiunta una gestione basilare dei parametri.
    Per ora esiste solo il parametro --find_limit
    che avvia la procedura per testare matrici di dimensione sempre crescente.

--- compitini/1_Cholesky-Factorization/main.py
+++ compitini/1_Cholesky-Factorization/main.py
@@ -1,12 +1,15 @@
 import tester as Tester
+import argparse
 
 
-def main():
-    # Tester.find_limit(seed=20)
-    
-    A, b = Tester.generate_data(size=5, seed=20)
+def main(args):
+    if args.find_limit:
+        Tester.find_limit(seed=20)
 
-    _ = Tester.solve(A, b)
+    else:
+        A, b = Tester.generate_data(size=5, seed=20)
+
+        _ = Tester.solve(A, b)
 
     """ A = np.array([
         [5.2, 3, 0.5, 1, 2],
@@ -19,5 +22,16 @@ def main():
     b = np.array([9.45, -12.20, 7.78, -8.1, 10.0], dtype=float) # termine noto 
     """
 
+
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--find_limit", 
+        action="store_true",
+        help="Avvia la procedura per testare la velocità di risoluzione con matrici di grandezza crescente"
+    )
+
+    args = parser.parse_args()
+
+    main(args)
