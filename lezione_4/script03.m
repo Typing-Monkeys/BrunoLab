f0aa017990deeccf0a99b23a6fe1d04e18b88087
Zecown <nicoloposta@gmail.com>
2022-10-21 13:08:08 +0200

    Rotazione di un solido con QR Factorization
    Bellissima rotazione di un cubo su un asse

--- lezione_4/script03.m
+++ lezione_4/script03.m
@@ -1,3 +1,30 @@
 clear
-load data04.m
-load data04c.m
+load data04c.m %frame del cubo
+%load data04t.m %frame del triangolo
+clf
+plot3(f1(1,:),f1(2,:),f1(3,:));hold on
+axis([-3 3 -3 3 -3 3]);
+[az el] = view;
+axis('equal')
+% generare una rotazione a caso 
+% (è generabile con fattorizzazione QR di matrice casuale)
+N = 0; t=0.1;
+for ell = 1:N
+    [Q R] = qr(randn(3));
+    f2 = Q*f1;
+    plot3(f2(1,:),f2(2,:),f2(3,:)); 
+    pause(t);
+end
+
+v = [1;0;0];
+N = 100; t = 0.05;
+theta = 1;
+G = [1 0 0;
+    0 cos(theta) -sin(theta);
+    0 sin(theta) cos(theta);];
+for ell = 1:N
+    f1 = G*f1;
+    plot3(f1(1,:),f1(2,:),f1(3,:)); 
+    pause(t);
+end
+    
