97711dd9c9345bae5f745e92c296e98a693d01fc
Nicolò Vescera <nicolo.vescera@gmail.com>
2022-10-24 17:45:58 +0200

    Rinominato File
    Camiato nome ad un file per rendere più chiaro il suo contenuto.
    
    cost_tester ➡️ tests

--- compitini/1_Cholesky-Factorization/tester/__init__.py
+++ compitini/1_Cholesky-Factorization/tester/__init__.py
@@ -1,4 +1,4 @@
 from .execution_time import get_execution_time
 from .data_generator import generate_data
-from .cost_tester import find_limit, simple_test
+from .tests import find_limit, simple_test
 from .logger import Logger
